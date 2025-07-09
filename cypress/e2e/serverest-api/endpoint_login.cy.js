72b94fb3fc3bdfe782709b68a6fed9d563ca5fa9
Dayene Sousa <sousadayene@gmail.com>
2025-07-08 22:20:49 -0300

    Adionar testes de login

---
+++ cypress/e2e/serverest-api/endpoint_login.cy.js
@@ -0,0 +1,56 @@
+
+//comando para abrir um servidor do serveRest: npx serverest@latest//
+
+describe('API - Teste Funcional de login', () => {
+    it(`Deve realizar o login com sucesso`, () =>{
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/login',
+            body: {
+                "email": "fulano@qa.com",
+                "password": "teste"
+            }  
+
+    }).then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('message', 'Login realizado com sucesso')
+        const auth = response.body.authorization
+        cy.log(response.body.authorization)
+        Cypress.env('authorization', auth);
+
+
+})
+})
+});
+
+
+
+
+
+
+
+
+
+
+
+it(`Deve informar mensagem de validação `, () =>{
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3000/login',
+        body: {
+            "email": "fuladddno@qa.com",
+            "password": "tsddvsss",
+        },
+        failOnStatusCode:false 
+
+        }).then((response) => {
+            // Verifica se a resposta tem status 400 (Bad Request)
+            expect(response.status).to.eq(401);
+      
+            // Verifica se a resposta contém a mensagem de erro esperada
+            expect(response.body).to.have.property('message', 'Email e/ou senha inválidos');
+          
+
+        })
+    
+    });
---
+++ cypress/support/commands.js
@@ -0,0 +1,42 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+Cypress.Commands.add('login_api',( user, password ) => {  
+            cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/login',
+            body: {
+
+            "email": user,
+            "password": password  
+            }
+        
+
+            }).then((response) => {
+                return response 
+            })
+        })
+
+
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
