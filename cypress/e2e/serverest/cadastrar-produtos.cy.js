5b8638842cf2c6be50befb2a8d05a2941eca45f7
Dayene Sousa <sousadayene@gmail.com>
2025-07-20 00:11:20 -0300

    Adicionando a opção fixture para trazer informações do body,  no cadastro de de alterar produto

--- cypress/e2e/serverest/cadastrar-produtos.cy.js
+++ cypress/e2e/serverest/cadastrar-produtos.cy.js
@@ -1,26 +1,44 @@
-
-
-
-describe('Cadastrar produtos', () => {
-    it(`Deve Cadastrar produtos com sucesso`, () =>{
+describe('API - Teste Funcional de login', () => {
+    it(`Deve realizar o login com sucesso`, () =>{
         cy.request({
+
+            method: 'POST',
+            url: 'http://localhost:3000/login',
+            body: {
+                "email": "fulano@qa.com",
+                "password": "teste"
+            }  
+
+    }).then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('message', 'Login realizado com sucesso')
+        const auth = response.body.authorization
+        cy.log(response.body.authorization)
+        Cypress.env('authorization', auth);
+
+
+})
+})
+
+
+
+    it(`Deve Cadastrar produtos com sucesso`, () =>{
+        cy.fixture('cadastrar-produto.json').then((json) => {
+        cy.request({
+            failOnStatusCode: false,
             method: 'POST',
             url: 'http://localhost:3000/produtos',
             headers: {
                'accept': 'application/json',
-               'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZ1bGFub0BxYS5jb20iLCJwYXNzd29yZCI6InRlc3RlIiwiaWF0IjoxNzUyMTA0NTgxLCJleHAiOjE3NTIxMDUxODF9.EkwbiKfZJHIIatguPuj8Y5bbW8BGkJXFnydS1qrH-UY',
+               'Authorization': Cypress.env('authorization'),
                'Content-Type': 'application/json'
             },
-            body: {
-                 "nome": "coleta",//produto deve ser sempre alterado
-                 "preco": 470,
-                 "descricao": "saude",
-                 "quantidade": 381 
-        
-            }
+            body: json
+            
 
     }).then((response) => {
         expect(response.status).to.equal(201)
     })
 })
-});
+})
+})
